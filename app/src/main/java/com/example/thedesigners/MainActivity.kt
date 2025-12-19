a3199398311579233816b0ebe97f684946a6efc2
Roshankumar404 <roshankuar951@gmail.com>
2025-12-19 22:37:17 +0530

    the changes are minor but imp one

--- app/src/main/java/com/example/thedesigners/MainActivity.kt
+++ app/src/main/java/com/example/thedesigners/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.thedesigners.ui.theme.TheDesignersTheme
@@ -56,6 +60,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun Texts(){
     Text(
         text = "You are the roshan", color = Color.Red,
+        modifier = Modifier.border(width = 10.dp, shape = RoundedCornerShape(10.dp), color = Color.Black).background(Color.Gray).padding(20.dp),
         fontSize = 32.sp,
         fontStyle = FontStyle.Italic
     )
@@ -65,6 +70,7 @@ fun Texts(){
 @Composable
 fun GreetingPreview() {
     TheDesignersTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        Texts()
     }
 }
