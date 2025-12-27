f7f8cdb81c6c63c4126c87e5d4086c02190cea39
Roshankumar404 <roshankuar951@gmail.com>
2025-12-27 11:10:37 +0530

    Modified the Screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
 }
--- app/src/main/java/com/example/thedesigners/Screens/AIEditor.kt
+++ app/src/main/java/com/example/thedesigners/Screens/AIEditor.kt
@@ -1,1 +1,35 @@
 package com.example.thedesigners.Screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.thedesigners.ui.theme.TheDesignersTheme
+
+@Composable
+fun AiEditor(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(modifier = Modifier.fillMaxSize()
+            .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Home", fontSize = 40.sp,color = Color.Green
+            )
+        }
+    }
+}
+@Preview
+@Composable
+fun Home(){
+    TheDesignersTheme {
+        AiEditor()
+    }
+}
