9a598d86b07e4f70bb388aa49470dd8b82161baf
Roshankumar404 <roshankuar951@gmail.com>
2025-12-31 15:40:57 +0530

    fixed bottom tab styling

--- app/src/main/java/com/example/thedesigners/BottomBar.kt
+++ app/src/main/java/com/example/thedesigners/BottomBar.kt
@@ -1,20 +1,30 @@
 package com.example.thedesigners
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsEndWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -29,6 +39,7 @@ import java.nio.file.WatchEvent
 @Composable
 fun Bottombar(navController: NavController)
 {
+    val context= LocalContext.current
     val currentRoute=navController.currentBackStackEntryAsState().value?.destination?.route
     BottomAppBar(containerColor = Green, ){
         IconButton(onClick = {
@@ -62,6 +73,38 @@ fun Bottombar(navController: NavController)
                 tint = if (currentRoute== SealedScreens.Profile.Screens) Color.White else Color.Green
             )
         }
+        Box(Modifier
+            .weight(1f)
+            .padding(15.dp)
+            , contentAlignment = Alignment.Center
+
+        ){
+            FloatingActionButton(onClick =
+                { Toast
+                    .makeText(context,"jai sri ram",
+                        Toast.LENGTH_SHORT)
+                    .show()} ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add", modifier = Modifier.size(30.dp), tint = Green)
+
+            }
+
+        }
+        IconButton(onClick = {
+            navController.navigate(SealedScreens.Search.Screens){
+                popUpTo(SealedScreens.Profile.Screens){
+                    inclusive=false
+                }
+            }
+         },
+            modifier = Modifier.weight(1f)
+         ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search",
+
+                tint = if (currentRoute== SealedScreens.Search.Screens) Color.White else Color.Green
+            )
+        }
         IconButton(onClick = {
             navController.navigate(SealedScreens.Search.Screens){
                 popUpTo(SealedScreens.Profile.Screens){
@@ -72,8 +115,8 @@ fun Bottombar(navController: NavController)
             modifier = Modifier.weight(1f)
         ) {
             Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Search",
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Liked",
 
                 tint = if (currentRoute== SealedScreens.Search.Screens) Color.White else Color.Green
             )
